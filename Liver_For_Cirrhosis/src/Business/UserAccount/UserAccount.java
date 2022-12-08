fba63e46d7286276d7a653697c138ac4a5a0d191
CY2404 <114796225+CY2404@users.noreply.github.com>
2022-12-08 03:27:05 -0500

    account attributes

--- Liver_For_Cirrhosis/src/Business/UserAccount/UserAccount.java
+++ Liver_For_Cirrhosis/src/Business/UserAccount/UserAccount.java
@@ -9,5 +9,59 @@ package Business.UserAccount;
  * @author cheril
  */
 public class UserAccount {
+    private String username;
+    private String password;
+    private Employee employee;
+    private Role role;
+    private WorkQueue workQueue;
+    private String name;
     
+    public UserAccount() {
+        workQueue = new WorkQueue();
+    }    
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
