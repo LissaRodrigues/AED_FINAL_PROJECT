d12fc571de372f2f772c33ad154e5dbcd22e60b7
CY2404 <114796225+CY2404@users.noreply.github.com>
2022-12-08 03:27:55 -0500

    directory

--- Liver_For_Cirrhosis/src/Business/UserAccount/UserAccountDirectory.java
+++ Liver_For_Cirrhosis/src/Business/UserAccount/UserAccountDirectory.java
@@ -9,5 +9,22 @@ package Business.UserAccount;
  * @author cheril
  */
 public class UserAccountDirectory {
+     private ArrayList<UserAccount> userAccountList;
+
+    public UserAccountDirectory() {
+        userAccountList = new ArrayList();
+    }
+
+    public ArrayList<UserAccount> getUserAccountList() {
+        return userAccountList;
+    }
     
+    public UserAccount authenticateUser(String username, String password){
+        for (UserAccount ua : userAccountList)
+            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)){
+                return ua;
+            }
+//        System.out.println("No user account found. Returning Null");
+        return null;
+    }
 }
